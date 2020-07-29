258124416a1a104e35924744c2a88dc03a25b672
chris <christopherchateau@gmail.com>
2020-07-29 11:44:34 -0600

    install vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <HelloWorld msg="sup" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+    name: "App",
+    components: {
+        HelloWorld,
+    },
+};
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+</style>
